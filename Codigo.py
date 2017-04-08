7373dd8be3533a1c49d1793cfe522dca4d663998
alessandrablucher@hotmail.com <alessandrablucher@hotmail.com>
2017-04-08 15:43:07 -0300

    Função de Batalha e espaçamento

--- Codigo.py
+++ Codigo.py
@@ -7,12 +7,53 @@ def mostra_ipmon(ipmon,a):
 	print("def = {0}".format(ipmon[a]["def"]))
 	print("hp = {0}\n".format(ipmon[a]["hp"])) 
 
+def batalha(inspermons, outros, SuasInfo, Insperdex):
+	adversario = str(random.choice(outros))
+	VidaAdversarioInicial = inspermons[adversario]["hp"]
+	print("Um inimigo quer batalhar...")
+	print("\n")
+	InfoAdversario = mostra_ipmon(inspermons,adversario)
+	Vontade = input("Digite N se deseja fugir da batalha, ou pressione Enter para continuar")
+	if Vontade == "N" or Vontade == "n":
+		print("\n")
+		print("Você fugiu da batalha")
+		return "fuga"
+	Inimigo = inspermons[adversario]
+	x = 0
+	while Inimigo["hp"] > 0  and SuasInfo["hp"] > 0:
+		if x%2 == 0:
+			TipodeAtk = input("Escolha o ataque que deseja usar: \n 1.{}".format(SuasInfo["Atk"][0]))
+			print("\n")
+			Inimigo["hp"] = Inimigo["hp"] - (SuasInfo["Atk"][1]-Inimigo["def"])
+			x += 1
+			if TipodeAtk == "1":
+				print("{} usou {}. Foi super efetivo!".format(SuasInfo["nome"],SuasInfo["Atk"][0]))
+				print("\n")
+				if Inimigo["hp"] <= 0:
+					print("Você ganhou a batalha!")
+					print("\n")
+					if inspermons[adversario] not in Insperdex:
+						Insperdex.append(inspermons[adversario])
+						print("Esse pokémon foi adicionado na sua pokédex.")
+						inspermons[adversario]["hp"] = VidaAdversarioInicial
+					return
+		else:
+			SuasInfo["hp"] = SuasInfo["hp"] - (Inimigo["Atk"][1]-SuasInfo["def"])
+			x += 1
+			print("{} usou {}. Foi super efetivo!".format(Inimigo["nome"],Inimigo["Atk"][0]))
+			print("\n")
+			if SuasInfo["hp"] <= 0:
+				print("Seu pokémon desmaiou, reviva ele no PokéCenter")
+				SuasInfo["hp"] = 0
+				return
+
 with open('Insper.json') as arquivo: 
 	inspermons = json.load(arquivo) 
 
-Inicio = input("Qual você escolhe para começar? \n 1.Bullbasaur \n 2.Charmander \n 3.Squirtle \n Digite o número: ")
+Inicio = input("--------------------------------------\n  * Qual você escolhe para começar? *\n-------------------------------------- \n 1.Bullbasaur \n 2.Charmander \n 3.Squirtle \n Digite o número: ")
 SuasInfo = inspermons[Inicio]
 VidaInicial = SuasInfo["hp"]
+print("\n")
 print("As suas informações são:")
 mostra_ipmon(inspermons, Inicio)
 Insperdex = [SuasInfo]
@@ -20,7 +61,7 @@ outros = list(range(1,11))
 
 
 while True:
-	Menu = input("1. Agir \n2.Ver Pokédex \n3. Administrar pokémons \n4.Status do Pokémon Principal")
+	Menu = input("1.Agir \n2.Ver Pokédex \n3.Administrar pokémons \n4.Status do Pokémon Principal \n Digite o número: ")
 	if Menu == "2":
 		for a in range(len(Insperdex)):
 			print("{}:".format(a+1))
@@ -35,52 +76,28 @@ while True:
 	if Menu == "4":
 		mostra_ipmon(Insperdex, int(SeuPokemon)-1)
 	if Menu == "1":
+		print("\n")
 		Fazer = input("O que você quer fazer? \n 1.Passear \n 2.Dormir? \n 3.Ir para um PokéCenter \n Digite o número: ")
+		print("\n")
 		if Fazer == "2":
 			print("Até a próxima!")
 			break
-		if Fazer == "3":
+		elif Fazer == "3":
 			Desejo = input("Nurse Joy: Oi! Deseja reviver seu pokémon?  S/N ")
 			if Desejo == "S" or Desejo == "s":
 				SuasInfo["hp"] = VidaInicial
+				print("\n")
 				print("A vida do seu pokémon foi restaurada!")
 				continue
 			if Desejo == "N" or Desejo == "n":
+				print("\n")
 				print("Volte sempre!")
 				continue
-		elif SuasInfo["hp"] > 0:
-			adversario = str(random.choice(outros))
-			VidaAdversarioInicial = inspermons[adversario]["hp"]
-			print("Um inimigo quer batalhar...")
-			InfoAdversario = mostra_ipmon(inspermons,adversario)
-			Vontade = input("Digite N se deseja fugir da batalha, ou pressione Enter para continuar")
-			if Vontade == "N" or Vontade == "n":
-				print("Você fugiu da batalha")
+		elif Fazer == "1":
+			v = batalha(inspermons, outros, SuasInfo, Insperdex)
+			if v == "fuga":
 				continue
-			Inimigo = inspermons[adversario]
-			x = 0
-			flag = 0
-			while flag == 0:
-				if x%2 == 0:
-					TipodeAtk = input("Escolha o ataque que deseja usar: \n 1.{}".format(SuasInfo["Atk"][0]))
-					Inimigo["hp"] = Inimigo["hp"] - (SuasInfo["Atk"][1]-Inimigo["def"])
-					x += 1
-					if TipodeAtk == "1":
-						print("{} usou {}. Foi super efetivo!".format(SuasInfo["nome"],SuasInfo["Atk"][0]))
-					if Inimigo["hp"] <= 0:
-						print("Você ganhou a batalha!")
-						flag = 1
-						if inspermons[adversario] not in Insperdex:
-							Insperdex.append(inspermons[adversario])
-							print("Esse pokémon foi adicionado na sua pokédex.")
-							inspermons[adversario]["hp"] = VidaAdversarioInicial
-				else:
-					SuasInfo["hp"] = SuasInfo["hp"] - (Inimigo["Atk"][1]-SuasInfo["def"])
-					x += 1
-					print("{} usou {}. Foi super efetivo!".format(Inimigo["nome"],Inimigo["Atk"][0]))
-					if SuasInfo["hp"] <= 0:
-						print("Seu pokémon desmaiou, reviva ele no PokéCenter")
-						flag = 1
-						SuasInfo["hp"] = 0
 		else:
 			print("Você nao pode batalhar com seu Pokémon desmaiado!")
+	print("\n")
+	print("**********************************************************")
